b174287e123b1cb2ae78f466b890619e4dcaa592
Jeremy Muhlich <jmuhlich@bitflood.org>
2011-05-19 02:21:49 -0400

    add some initial plotting

--- lsbacct_plot_user_week.py
+++ lsbacct_plot_user_week.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
 import sys
-import rrdtool
+from collections import defaultdict
+from pylab import *
+from matplotlib.dates import DateFormatter
 from lsftools.logfile import LogfileReader
 
 if len(sys.argv) >= 2:
@@ -11,5 +13,19 @@ else:
 
 reader = LogfileReader(input_file)
 
+time_resolution = 60  # in seconds
+data = defaultdict(lambda: defaultdict(int))
 for row_num, record in enumerate(reader):
-    print row_num, record['userName']
+    start_time = record['startTime'] / time_resolution
+    end_time = record['eventTime'] / time_resolution
+    if start_time == 0:
+        continue
+    for t in range(start_time, end_time + 1):
+        data[record['userName']][t] += record['numExHosts']
+
+for name in sorted(data.keys()):
+    plot_date(array(data[name].keys()) * time_resolution, data[name].values(), label=name)
+legend()
+gca().set_yscale('log')
+#gca().get_xaxis().set_major_formatter(DateFormatter('%b %d'))
+show()
